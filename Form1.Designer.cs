3d52e6b0527292c5062e54f19eac10a5866870ae
cukw <kan142006@gmail.com>
2025-01-23 22:10:10 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Azino777
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.textBox1.Enabled = false;
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox1.Location = new System.Drawing.Point(133, 71);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(26, 45);
+			this.textBox1.TabIndex = 0;
+			// 
+			// textBox2
+			// 
+			this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.textBox2.Enabled = false;
+			this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox2.Location = new System.Drawing.Point(183, 71);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(26, 45);
+			this.textBox2.TabIndex = 1;
+			// 
+			// textBox3
+			// 
+			this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.textBox3.Enabled = false;
+			this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox3.Location = new System.Drawing.Point(234, 71);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(26, 45);
+			this.textBox3.TabIndex = 2;
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button1.Location = new System.Drawing.Point(133, 368);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(127, 44);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "Дэб в казик";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button2.Location = new System.Drawing.Point(372, 0);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(30, 32);
+			this.button2.TabIndex = 5;
+			this.button2.Text = "X";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = global::Игра.Properties.Resources._26b007f0a5ae3317b79d92921ce19cb4;
+			this.pictureBox1.Location = new System.Drawing.Point(116, 200);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(165, 162);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+			this.pictureBox1.TabIndex = 4;
+			this.pictureBox1.TabStop = false;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label1.Location = new System.Drawing.Point(147, 28);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(103, 25);
+			this.label1.TabIndex = 6;
+			this.label1.Text = "Azino777";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label2.Location = new System.Drawing.Point(66, 152);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(279, 25);
+			this.label2.TabIndex = 7;
+			this.label2.Text = "СЧАСТЛИВАЯ СЕМЕРКА!!!";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(403, 475);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Button button1;
+		internal System.Windows.Forms.TextBox textBox1;
+		internal System.Windows.Forms.TextBox textBox2;
+		internal System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using Игра.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace Azino777
+{
+	public partial class Form1 : Form
+	{
+		private readonly Random random = new Random();
+		public Form1()
+		{
+			InitializeComponent();
+			this.StartPosition = FormStartPosition.CenterScreen;
+			this.ControlBox = false;
+			pictureBox1.Visible = false;
+			label2.Visible = false;
+			
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			DialogResult res = MessageBox.Show("Вы уверены?","Выход из приложения", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+			if (res == DialogResult.Yes)
+			{
+				Application.Exit();
+			}
+		}
+		private async Task StartCasino(TextBox textBox)
+		{
+			for(int i = 0; i< 5; i++)
+			{
+				textBox.Text = random.Next(1, 10).ToString();
+				await Task.Delay(100);
+				
+			}
+		}
+		private async void button1_Click(object sender, EventArgs e)
+		{
+			button1.Enabled = false;
+			label2.Visible = false;
+			var list = new List<TextBox> { textBox1, textBox2, textBox3 };
+			foreach (TextBox textBox in list)
+			{
+				textBox.Clear();
+			}
+			pictureBox1.Visible = false;
+
+			foreach (TextBox textBox in list)
+			{
+				await StartCasino(textBox);
+			}
+
+			if (list.Any(item => item.Text=="7"))
+			{
+				pictureBox1.Visible = true;
+				label2.Visible = true ;
+			}
+			else
+			{
+				MessageBox.Show("Анлак(", "Не повезло");	 
+			}
+			button1.Enabled = true;
+			
+		}
+	}
+}
